54e6649867198b721c9e1da5ebb950627b23697c
Wallxxx <tvaibabyshka@gmail.com>
2022-08-31 23:17:57 +0300

    добавление кода

---
+++ src/main/java/com/calendar/dao/MeetingRepository.java
@@ -0,0 +1,14 @@
+package com.calendar.dao;
+
+import com.calendar.model.MeetingEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MeetingRepository extends JpaRepository<MeetingEntity, Long> {
+
+    @Query(value = "select u from MeetingEntity u where lower(u.name) = lower(:name)")
+    List<MeetingEntity> findByName(@Param("name") String name);
+}
